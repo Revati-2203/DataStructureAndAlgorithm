7f1b057b0dc7adb1cedee82330b359256b099b45
Revati-2203 <revati.u03@gmail.com>
2025-05-21 20:49:09 +0530

    maximumSumSubarrayOfSizeK

--- SlidingWindowFixedSize.java
+++ SlidingWindowFixedSize.java
@@ -34,6 +34,34 @@ class SlidingWindowFixedSize {
         }
         return ans;
     }
-    
+
+
+    //maximum sum subarray  of size k
+    public static int maximumSumSubarrayOfSizeK(int list[], int k){
+        if(list.length==0){
+            return 0;
+        }else{
+            int i=0, j=0;
+            int sum=0, maxSum =0;
+            while(j<list.length){
+                sum+=list[j];
+                if(j+1<k){
+                    j++;
+                }else if(j-i+1==k){
+                    maxSum=Math.max(maxSum, sum);
+                    sum-=list[i];
+                    i++;
+                    j++;
+                }
+            }
+            return maxSum;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] list = {1, 2, 3, 4, 5};
+        int k=3;
+        System.out.println("ans "+maximumSumSubarrayOfSizeK(list,k));
+    }
 }
     
